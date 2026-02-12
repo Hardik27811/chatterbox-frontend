bf063aedece8d32bf8d76a46882f52c9b011f913
Hardik Sharma <kumarankit2002dseu@gmail.com>
2026-02-12 17:56:41 +0530

    added deploy be url

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,6 @@
 import {io} from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_API_BASE_URL,{
+const socket = io("https://chatterbox-backend-smpj.onrender.com",{
     withCredentials : true
 });
 
